536cbb007b521ee731e6537bca2d7f7d87db1e62
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-08-09 06:25:20 +0600

    Making correction in the code

--- A_Phone_Desktop.cpp
+++ A_Phone_Desktop.cpp
@@ -10,57 +10,6 @@ int main()
     {
         int smallAppNumber, bigAppNumber;
         cin >> smallAppNumber >> bigAppNumber;
-
-        if (smallAppNumber == 0 && bigAppNumber == 0)
-        {
-            cout << 0 << endl;
-            continue;
-        }
-
-        int totalAppSize = 15;
-        int requiredScreen = 0;
-
-        int bigToSmall = bigAppNumber * 4;
-        if (bigToSmall >= 8)
-        {
-            while (bigToSmall >= 4)
-            {
-                requiredScreen++;
-                bigToSmall = bigToSmall - 8;
-            }
-
-            int vacantSpace = (requiredScreen * 15) - (bigAppNumber * 4) - smallAppNumber;
-
-            if (vacantSpace >= 0)
-            {
-                cout << requiredScreen << endl;
-                continue;
-            }
-            else
-            {
-                cout << requiredScreen + 1 << endl;
-                continue;
-            }
-        }
-        else
-        {
-            int currentAppSize = smallAppNumber + bigToSmall;
-            requiredScreen = 1;
-            if (currentAppSize > totalAppSize)
-            {
-                while (currentAppSize >= 15)
-                {
-                    requiredScreen++;
-                    currentAppSize = currentAppSize - 15;
-                }
-
-                cout << requiredScreen << endl;
-            }
-            else
-            {
-                cout << requiredScreen << endl;
-            }
-        }
     }
 
     return 0;
