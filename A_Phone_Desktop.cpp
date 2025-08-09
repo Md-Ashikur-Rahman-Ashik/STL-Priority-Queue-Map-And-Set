3e1b7e34e98b89e975bede6e43e86b56441fbeed
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-08-09 07:04:15 +0600

    Still solving phone desktop problem

--- A_Phone_Desktop.cpp
+++ A_Phone_Desktop.cpp
@@ -10,6 +10,48 @@ int main()
     {
         int smallAppNumber, bigAppNumber;
         cin >> smallAppNumber >> bigAppNumber;
+
+        if (smallAppNumber == 0 && bigAppNumber == 0)
+        {
+            cout << 0 << endl;
+            continue;
+        }
+
+        int requiredScreen = 0;
+        int bigAppRequirement = bigAppNumber / 2;
+
+        int vacantSpace = (bigAppRequirement * 15) - (bigAppNumber * 4);
+
+        if (bigAppNumber % 2 == 1)
+        {
+            bigAppRequirement++;
+        }
+
+        int smallAppRequirement = 0;
+
+        if (smallAppNumber > vacantSpace)
+        {
+            smallAppNumber = smallAppNumber - vacantSpace;
+            if (smallAppNumber > 15)
+            {
+                smallAppRequirement = smallAppNumber / 15;
+            }
+            else
+            {
+                smallAppRequirement = 1;
+            }
+        }
+
+        requiredScreen = bigAppRequirement + smallAppRequirement;
+
+        if (bigAppRequirement == 0 && smallAppRequirement == 0)
+        {
+            cout << 1 << endl;
+        }
+        else
+        {
+            cout << requiredScreen << endl;
+        }
     }
 
     return 0;
